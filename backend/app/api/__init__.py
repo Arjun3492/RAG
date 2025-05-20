04e61562e768546b2f2bb8e410743ef915a81858
arjun3492 <arjun.kohli.330@gmail.com>
2025-05-21 00:10:24 +0530

    feat:added health check

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,8 +1,10 @@
 # app/api/__init__.py
 from fastapi import APIRouter
-from app.api import upload, documents, ask
+from app.api import upload, documents, ask,health
 
 router = APIRouter()
 router.include_router(upload.router, prefix="/upload-documents", tags=["Upload"])
 router.include_router(documents.router, prefix="/documents", tags=["Documents"])
 router.include_router(ask.router, prefix="/ask", tags=["Query"])
+router.include_router(health.router, prefix="/health", tags=["Health"])
+
---
+++ backend/app/api/health.py
@@ -0,0 +1,8 @@
+# app/api/health.py
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/", tags=["Health"])
+def health_check():
+    return {"status": "ok"}
